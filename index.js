95d6aec859b2c6c620c1f369ac334595400fad47
Musa Maphutha <mumaphujh024@student.wethinkcode.co.za>
2026-03-03 19:16:01 +0200

    first commit: Added 70% of the functionality, more to be added

---
+++ index.js
@@ -0,0 +1,109 @@
+import { tweetsData } from "./data.js"
+ 
+
+const feed = document.getElementById("feed")
+const tweetBtn = document.getElementById("tweet-btn")
+const tweetInput = document.getElementById("tweet")
+
+
+document.addEventListener("click",function(e){
+
+    console.log(e.target.dataset)
+    
+})
+
+tweetBtn.addEventListener("click",addTweet)
+
+function addTweet(){
+
+    if(tweetInput.value != ""){
+        tweetsData.unshift({
+        handle: "@TROID",
+        profilePic: "images/scrimbalogo.png",
+        likes:0,
+        retweets: 0,
+        tweetText: tweetInput.value,
+        replies: [],
+        isRetweeted: false,
+        isLiked: false,
+        uuid: crypto.randomUUID(),
+
+    })
+
+    render()
+    tweetInput.value = ""
+    }
+    
+
+    
+
+
+
+}
+
+function handleLikeClick(tweeId){
+
+    
+
+
+}
+
+function handleRetweetClick(tweeId){
+
+    
+}
+
+function handleCommentClick(tweetId){
+
+}
+
+
+function getFeedHtml(){
+    let render = ""
+    tweetsData.forEach(function(tweet){
+
+
+        render += `
+                <div class="tweet">
+                    <div class="tweet-inner">
+                        <img src="${tweet.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${tweet.handle}</p>
+                            <p class="tweet-text">${ tweet.tweetText}</p>
+                            <div class="tweet-details">
+                                <span class="tweet-detail">
+                                    <i class="fa-regular fa-comment"  data-comment=${tweet.uuid}> </i>
+                                    ${tweet.replies.length}
+                                </span>
+                                <span class="tweet-detail">
+                                    <i class="fa-regular fa-heart" data-like=${tweet.uuid}> </i>
+                                    ${tweet.likes}
+                                </span>
+                                <span class="tweet-detail">
+                                    <i class="fa-solid fa-retweet" data-retweet=${tweet.uuid}> </i>
+                                    ${tweet.retweets}
+                                </span>
+                            </div>   
+                        </div>            
+                    </div>
+                </div>
+            
+            `
+        })
+
+        return render
+     
+
+}
+
+function render(){
+
+    const renderHtml = getFeedHtml()
+    feed.innerHTML = renderHtml
+    
+
+}
+
+
+render()
+
